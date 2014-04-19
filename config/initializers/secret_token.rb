c68452b5eeb9dd3fbb21c15c0f676aa8ac0cd498
Jonathan Chen <jxmoneyz@gmail.com>
2014-04-19 15:02:36 -0400

    First Commit, created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hackbyrd::Application.config.secret_key_base = 'bb60456dc9212191d5498ab639ff5a6a8d086a0d35443fccb20865356058edc95705651dc899ff1de1848b00dc89cf851bcc39ef88a1a8690c1138500d601de7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hackbyrd::Application.config.session_store :cookie_store, key: '_Hackbyrd_session'
