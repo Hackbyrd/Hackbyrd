ed7faaa4f54869246b41ec17412212584180cf71
Jonathan Chen <jonathan@fiscalnote.com>
2014-09-21 11:41:51 -0400

    put stars

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -6,6 +6,8 @@ $(function(){
   // If user has not scrolled down yet, make
   // $("#stop-waiting").delay(5000).fadeIn();
 
+  // $("#jonathan-chen-svg").css("background", "background: rgba(79, 140 ,247, 0);")
+
   //set the units for the svg paths
   $(".jonathan_path").css("stroke-dashoffset", "1000");
   $(".jonathan_path").css("stroke-dasharray", "1000");
@@ -20,7 +22,7 @@ $(function(){
     if ($(window).height() + $(window).scrollTop() < $("html").height()) {
 
       //calculate how far down the page the user is
-      var $percentageComplete = (($(window).scrollTop()/($("html").height() - $(window).height()))*50);
+      var $percentageComplete = (($(window).scrollTop()/($("html").height() - $(window).height())) * 36);
 
       //convert dashoffset pixel value to integer
       var $newUnit = parseInt($dashOffset, 10);
@@ -34,7 +36,11 @@ $(function(){
       //set the new value of the dashoffset to create the drawing effect
       $(".jonathan_path").css("stroke-dashoffset", $newUnit - $offsetUnit);//.star
 
-    } else { // if scroll is greater or equal to window height
+    } else { // if scroll is greater or equal to window height divided by two (halfway point)
+
+      // var left_offset = $(window).height()/2 - $(window).scrollTop();
+
+      // $("#jonathan-chen-svg").css("left", left_offset);
 
     } // end check scroll vertical
 
