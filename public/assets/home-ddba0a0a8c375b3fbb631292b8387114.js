cf6c7b2048d3ae7c21f10be37ea2b9e755a1e89e
Jonathan Chen <jonathan@fiscalnote.com>
2014-10-13 23:59:02 -0400

    precompile assets

---
+++ public/assets/home-ddba0a0a8c375b3fbb631292b8387114.js
@@ -0,0 +1,50 @@
+/* Hackbyrd javascript */
+
+// Javascript
+$(function() {
+
+  // If user has not scrolled down yet, make
+  // $("#stop-waiting").delay(5000).fadeIn();
+
+  // $("#jonathan-chen-svg").css("background", "background: rgba(79, 140 ,247, 0);")
+
+  //set the units for the svg paths
+  $(".jonathan_path").css("stroke-dashoffset", "1000");
+  $(".jonathan_path").css("stroke-dasharray", "1000");
+
+  //variable for the 'stroke-dashoffset' unit
+  var $dashOffset = $(".jonathan_path").css("stroke-dashoffset");//.star
+
+  //on a scroll event - execute function
+  $(window).scroll(function() {
+
+    // check if there is space left to scroll vertical
+    if ($(window).height() + $(window).scrollTop() < $("html").height()) {
+
+      //calculate how far down the page the user is
+      var $percentageComplete = (($(window).scrollTop()/($("html").height() - $(window).height())) * 36);
+
+      //convert dashoffset pixel value to integer
+      var $newUnit = parseInt($dashOffset, 10);
+
+      //convert percentage complete unit to an interger
+      //var $parsedPercentage = parseInt($percentageComplete, 10);
+
+      //get the value to be subtracted from the 'stroke-dashoffset'
+      var $offsetUnit = $percentageComplete * ($newUnit / 100);
+
+      //set the new value of the dashoffset to create the drawing effect
+      $(".jonathan_path").css("stroke-dashoffset", $newUnit - $offsetUnit);//.star
+
+    } else { // if scroll is greater or equal to window height divided by two (halfway point)
+
+      $(".jonathan_path").css("stroke-dashoffset", 0);
+
+      // var left_offset = $(window).height()/2 - $(window).scrollTop();
+      // $("#jonathan-chen-svg").css("left", left_offset);
+
+    } // end check scroll vertical
+
+  });
+
+});
---
+++ public/assets/svg_draw-2fdefed62433bfb732acdab65e3d18c8.js
@@ -0,0 +1,56 @@
+//http://codepen.io/anon/pen/idvoh
+
+//On scroll call the draw function
+$(window).scroll(function() {
+  drawLines();
+  console.log("cat");
+});
+
+//If you have more than one SVG per page this will pick it up
+function drawLines() {
+  console.log("dog");
+
+  // var array = $(".timeline-draw");
+
+  // for (var i = 0; i < array.length; i++) {
+  //   drawLine($("#route"), array[i]);
+  // }
+
+  $.each($("timeline-draw"), function(i, val) {
+    var line = val;
+    drawLine($(this), line);
+  });
+}
+
+//draw the line
+function drawLine(container, line) {
+  console.log("fish");
+  var length = 0;
+  var pathLength = line.getTotalLength();
+  var distanceFromTop = container.offset().top - $(window).scrollTop();
+  var percentDone = 1 - (distanceFromTop / $(window).height());
+  length = percentDone * pathLength;
+  line.style.strokeDasharray = [length,pathLength].join(' ');
+  console.log("strokeDasharray: " + [length,pathLength].join(' '));
+}
+
+// $(document).ready(function(){
+
+//   $(window).scroll(function() {
+//     drawLine( $('#route'), document.getElementById('timeline-draw') );
+//   });
+
+//   // init the line length
+//     drawLine( $('#route'), document.getElementById('timeline-draw') );
+
+//   //draw the line
+//   function drawLine(container, line){
+
+//     var pathLength = line.getTotalLength();
+//     var maxScrollTop = $(document).height() - $(window).height();
+//     var percentDone = $(window).scrollTop() / maxScrollTop;
+//     var length = percentDone * pathLength;
+//     line.style.strokeDasharray = [ length ,pathLength].join(' ');
+//   }
+
+// });
